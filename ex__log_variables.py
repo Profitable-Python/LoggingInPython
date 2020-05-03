cb4561edebbeef538740a2d28e40ef197b868082
bfmcneill <ben@profitablepython.fm>
2020-05-03 11:31:05 -0600

    completed example - logging a variable

---
+++ ex__log_variables.py
@@ -0,0 +1,9 @@
+import logging
+
+
+# username = "ben@profitablepython.fm"
+# username = "name@example.email"
+
+username = input("What is your username? ")
+
+logging.error(f"{username} raised an error")
